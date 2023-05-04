dac567d96a0d7be6394e058f5dc84501903bfd30
TheMasterofDeezNuts <132610491+TheMasterofDeezNuts@users.noreply.github.com>
2023-05-04 20:58:33 +0200

    ???

---
+++ Prog-Eksamenssæt/Shared/Schedule.cs
@@ -0,0 +1,20 @@
+﻿namespace Prog_Eksamenssæt.Shared
+{
+	public class Schedule
+	{
+		public Event[] events { get; set; }
+
+	}
+	public class Event
+	{
+		public string summary { get; set; }
+		public string location { get; set; }
+		public string dtstart { get; set; }
+		public string dtend { get; set; }
+		public string description { get; set; }
+
+		public DateTime Start { get; set; }
+		public DateTime Slut { get; set; }
+
+	}
+}
